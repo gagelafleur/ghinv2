67403fed58b8075021d2b4b92bf342e9d608a9bd
Gage LaFleur <gpl9310@gmail.com>
2021-02-18 16:31:55 -0500

    added checks that user has adequate permissions when posting a score

--- app/Http/Controllers/ScoreController.php
+++ app/Http/Controllers/ScoreController.php
@@ -89,58 +89,43 @@ class ScoreController extends Controller
 
     public function getAddScore(){
 
-    $courses = Course::orderBy('name')->where('state','NY')->get();
-    $accessingUser = Auth::User();
+      $accessingUser = Auth::User();
 
-    $sticky = [];
-    $others = [];
-
-    foreach($courses as $course){
-
-      if($course->course_name === 'Winged Pheasant'){
-
-        $sticky[] = $course;
-
-      }else{
-
-        $others[] = $course;
-
+      if(!$accessingUser->hasRole('score_poster')){
+        abort(403);
       }
+
+      return view('score.add', ['user' => $accessingUser]);
 
     }
 
-    $courses = array_merge($sticky, $others);
+    public function postSaveCourse(Request $request){
+      //dd($request);
+      $this->validate($request, [
 
-    return view('score.add', ['user' => $accessingUser, 'courses' => $courses]);
+        'score' => 'required|numeric|max:200',
+        'date' => 'required|date',
+        'courseHidden' => 'required|numeric',
+        'tee' => 'required|numeric',
 
+      ]);
 
-  }
+      $accessingUser = Auth::User();
 
-  public function postSaveCourse(Request $request){
-    //dd($request);
-    $this->validate($request, [
+      if(!$accessingUser->hasRole('score_poster')){
+        abort(403);
+      }
 
-      'score' => 'required|numeric|max:200',
-      'date' => 'required|date',
-      'courseHidden' => 'required|numeric',
-      'tee' => 'required|numeric',
+      $score = new Score();
+      $score->score = $request->score;
+      $score->played_on = $request->date;
+      $score->course_id = $request->courseHidden;
+      $score->tee = $request->tee;
+      $score->user_id = $accessingUser->id;
+      $score->save();
 
-    ]);
-
-    $accessingUser = Auth::User();
-
-    $score = new Score();
-    $score->score = $request->score;
-    $score->played_on = $request->date;
-    $score->course_id = $request->courseHidden;
-    $score->tee = $request->tee;
-    $score->user_id = $accessingUser->id;
-    $score->save();
-
-    //Activity::log($accessingUser->name." Posted a Score.");
-
-    return redirect()->route('home')->with(['success' => 'Score Successfuly Added']);
+      return redirect()->route('home')->with(['success' => 'Score Successfuly Added']);
 
 
-  }
+    }
 }
