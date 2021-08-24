6121e6a7c49e5b7923b6dba2ede2052821fcabe9
Gage LaFleur <gpl9310@gmail.com>
2021-08-24 15:44:35 -0400

    trying to set db host manually

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST'),
+            'host' => env('DB_HOST','aaot3jg987aljz.ccpzpmfo7atb.ca-central-1.rds.amazonaws.com'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE'),
             'username' => env('DB_USERNAME'),
