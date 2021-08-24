b243f9f45912d34ce94e700d57dc676fc2e7a63e
Gage LaFleur <gpl9310@gmail.com>
2021-08-24 18:08:04 -0400

    dumb mistake i think

--- .platform/hooks/postdeploy/01_move_some_files.sh
+++ .platform/hooks/postdeploy/01_move_some_files.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 # Move the files you created in /tmp into the desired directories.
-#sudo mv /home/ec2-user/.env /var/www/html/.env
+sudo mv /home/ec2-user/.env.elasticbeanstalk /var/www/html/.env
 
 # other bash commands
