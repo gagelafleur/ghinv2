ad310b9900c1d72e20ef3cb5d40f560c14785b1a
Gage LaFleur <gpl9310@gmail.com>
2021-08-24 19:41:29 -0400

    changing owner and group for .env

--- .platform/hooks/postdeploy/01_copy_env.sh
+++ .platform/hooks/postdeploy/01_copy_env.sh
@@ -2,5 +2,6 @@
 
 # Move the files you created in /tmp into the desired directories.
 /bin/cp /home/ec2-user/.env.elasticbeanstalk /var/www/html/.env
-
+/bin/chown webapp /var/www/html/.env
+/bin/chgrp webapp /var/www/html/.env
 # other bash commands
